8b96ed57ff7acfe4bf1dd64322f2c8f648f604e5
Nahid <nahidmarfy07@gmail.com>
2022-03-13 00:06:24 +0600

    Final marge

---
+++ Circular_Bus_App/Controllers/BusOwnerController.cs
@@ -0,0 +1,161 @@
+﻿using Circular_Bus_App.Auth;
+using Circular_Bus_App.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Circular_Bus_App.Controllers
+{
+    [BusOwnerAccess]
+    public class BusOwnerController : Controller
+    {
+        // GET: BusOwner
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BusOwnerProfile()
+        {
+            string loggedUserName = Session["username"].ToString();
+            string id = Session["userid"].ToString();
+            int loggedUserId = Int32.Parse(id);
+            CircularBusEntities db = new CircularBusEntities();
+
+            var data = (from e in db.Users
+                        where e.U_Id.Equals(loggedUserId) &&
+                        e.U_UserName.Equals(loggedUserName)
+                        select e).ToList();
+            return View(data);
+        }
+
+        public ActionResult Buses()
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            int s = (int)Session["userid"];
+            var data = (from u in db.BusInfoes
+                        where u.B_OwnedBy == s && u.B_Status == "Active              "
+                        select u).ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult BusOwnerEdit(int id)
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            var data = (from u in db.Users
+                        where u.U_Id == id
+                        select u).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult BusOwnerEdit(User new_User)
+        {
+            if (ModelState.IsValid)
+            {
+                CircularBusEntities db = new CircularBusEntities();
+                var data = (from u in db.Users
+                            where u.U_Id == new_User.U_Id
+                            select u).FirstOrDefault();
+                db.Entry(data).CurrentValues.SetValues(new_User);
+                db.SaveChanges();
+                return RedirectToAction("Index", "BusOwner");
+
+            }
+            return View();
+        }
+        public ActionResult BusEdit(int id)
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            var data = (from u in db.BusInfoes
+                        where u.B_Id == id
+                        select u).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult BusEdit(BusInfo b)
+        {
+            if (ModelState.IsValid)
+            {
+                CircularBusEntities db = new CircularBusEntities();
+                var data = (from u in db.BusInfoes
+                            where u.B_Id == b.B_Id
+                            select u).FirstOrDefault();
+                db.Entry(data).CurrentValues.SetValues(b);
+                db.SaveChanges();
+                return RedirectToAction("Buses", "BusOwner");
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BusOwnerDelete(int id)
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            var data = (from u in db.Users
+                        where u.U_Id == id
+                        select u).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult BusOwnerDelete(User delete)
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            var data = (from u in db.Users
+                        where u.U_Id == delete.U_Id
+                        select u).FirstOrDefault();
+            db.Entry(data).CurrentValues.SetValues(delete);
+            db.Users.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("LoginUser");
+
+        }
+
+        public ActionResult LogoutUser()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LoginUser");
+        }
+        public ActionResult AddBus()
+        {
+            return View(new BusInfo());
+        }
+        [HttpPost]
+        public ActionResult AddBus(BusInfo u)
+        {
+            if (ModelState.IsValid)
+            {
+                CircularBusEntities db = new CircularBusEntities();
+                db.BusInfoes.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("Buses", "BusOwner");
+            }
+            return View(u);
+        }
+
+        public ActionResult ViewSell()
+        {
+            CircularBusEntities db = new CircularBusEntities();
+            int s = (int)Session["userid"];
+
+            var sum_price = (from sum in db.Carts
+                             join b in db.BusInfoes on sum.B_Id equals b.B_Id
+                             where sum.B_Id == b.B_Id && b.B_OwnedBy == s
+                             select sum).Sum(S => S.BS_Fair).ToString();
+            ViewBag.SellPrice = sum_price;
+            var tbl = (from sum in db.Carts
+
+                       where sum.BusInfo.B_OwnedBy == s
+                       select sum).ToList();
+
+            return View(tbl);
+        }
+    }
+}
